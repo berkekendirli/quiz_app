05522a7ad2b1069668526cf91c0fd532ac4f6a80
berkekendirli <efekndrl@gmail.com>
2024-02-22 19:10:27 +0300

    commit2

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -9,25 +10,41 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  var currentQuestionIndex = 0;
+  void answerQuestion(){
+    currentQuestionIndex++;
+
+
+  }
+
+
   @override
   Widget build(BuildContext context) {
+    final currentQuestion = questions[1];
+
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            'Question 1',
-            style: TextStyle(fontSize: 24, color: Colors.white),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          AnswerButton('Answer 1', () => null),
-          AnswerButton('Answer 2', () => null),
-          AnswerButton('Answer 3', () => null),
-          AnswerButton('Answer 4', () => null),
-        ],
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(
+                color: Colors.white
+                ),
+                textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ...currentQuestion.getShuffledAnswers().map(
+              (answer) => AnswerButton(answer, () => null),
+            ),
+          ],
+        ),
       ),
     );
   }
